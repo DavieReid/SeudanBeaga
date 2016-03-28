5e801ca0f994f4bde632e7018764ba0285bb0f56
David Reid <david.reid@aggreko.co.uk>
2016-03-28 14:08:48 +0000

    routing set up.
     placeholder for gallery and contact pages.
    
    mocked up homepage

---
+++ SeudanBeaga/App/app.routing.js
@@ -0,0 +1,33 @@
+﻿function config($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise(function ($injector, $location) {
+        var $state = $injector.get("$state");
+        $state.go("home");
+    });
+
+   
+    $stateProvider
+        .state('home', {
+            url: "/index",
+            templateUrl: "../App/Main/main.html"
+        })
+        .state('gallery', {
+            url: "/gallery",
+            templateUrl: "App/Gallery/gallery.html",
+        })
+        .state('contact', {
+            url: "/contact",
+            templateUrl: "App/Contact/contact.html",
+        });
+
+    return {
+        appErrorPrefix: '[Error] ',
+        version: '1.0'
+    };
+}
+angular
+    .module('sbApp')
+    .config(config)
+    .run(function ($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
