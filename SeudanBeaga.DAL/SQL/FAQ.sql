28667c29eb50dfab9aaa5919f93b78ecf0d15fb7
David Reid <david.reid@aggreko.co.uk>
2016-05-02 17:04:21 +0000

    FAQ questions and answers insert sql script

---
+++ SeudanBeaga.DAL/SQL/FAQ.sql
@@ -0,0 +1,58 @@
+USE [SeudanBeaga]
+GO
+
+INSERT INTO [dbo].[Faq]
+           ([Question]
+           ,[Answer]
+           ,[Order])
+     VALUES
+           ('When do I need to pay my fees?'
+           ,'Fees must be paid weekly or monthly in advance.'
+           ,1)
+
+INSERT INTO [dbo].[Faq]
+           ([Question]
+           ,[Answer]
+           ,[Order])
+     VALUES
+           ('Do you accept childcare vouchers?'
+           ,'Yes we accept all types of childcare vouchers.'
+           ,2)
+
+INSERT INTO [dbo].[Faq]
+           ([Question]
+           ,[Answer]
+           ,[Order])
+     VALUES
+           ('What methods of payments are accepted?'
+           ,'We accept cash, cheque, standing order or childcare vouchers.'
+           ,3)
+
+INSERT INTO [dbo].[Faq]
+([Question]
+,[Answer]
+,[Order])
+VALUES
+('What do I need to bring for my child?'
+,'We ask that all children have a spare change of clothes which can remain in the nursery. We ask parents to provide nappies and wipes. During the summer months we ask parents to provide sun cream
+and a sun hat and in winter months we ask parents to provide suitable outdoor clothing and footwear.'
+,4)
+
+INSERT INTO [dbo].[Faq]
+           ([Question]
+           ,[Answer]
+           ,[Order])
+     VALUES
+           ('Can you administer medication to my child?'
+           ,'Yes, we can but the medication must be prescribed by a GP and have a dispensing date within 30 days of the date the medication has be administered. Parents are required to complete a medication
+form and sign the medication in in the morning and out at the end of the day.'
+           ,5)
+
+		   INSERT INTO [dbo].[Faq]
+           ([Question]
+           ,[Answer]
+           ,[Order])
+     VALUES
+           ('Can I phone throughout the day to see how my child is?'
+           ,'Yes, we have an open door policy and parents can phone throughout the day.'
+           ,6)
