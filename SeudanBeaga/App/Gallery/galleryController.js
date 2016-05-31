27a69c75a407b37430bb763e0352802be5a54edb
David Reid <david.reid@aggreko.co.uk>
2016-05-31 20:49:10 +0000

    new clock image added for opening hours section on main page. Gallery controller and datacontext retrieving data from api

---
+++ SeudanBeaga/App/Gallery/galleryController.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+
+    var controllerId = "galleryController";
+    angular.module("sbApp")
+        .controller(controllerId, ["gallery_datacontext", galleryController]);
+
+    function galleryController(gallery_datacontext) {
+
+        var vm = this;
+        vm.photos = null;
+
+        vm.getPhotos = getPhotos;
+
+
+        function getPhotos() {
+            if (vm.photos === null) {
+                gallery_datacontext.getGallery().then(function (response) {
+                    vm.photos = response.data;
+                });
+            }             
+        }
+    }
+})();
---
+++ SeudanBeaga/App/Gallery/gallery_datacontext.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+
+    var serviceId = "gallery_datacontext";
+    angular.module("sbApp")
+        .factory(serviceId, ["$http", "_apiUrl", gallery_datacontext]);
+
+    function gallery_datacontext($http, _apiUrl) {
+
+        var factory = {
+            getGallery: getGallery
+        };
+
+
+        return factory;
+
+        function getGallery() {
+
+            var url = _apiUrl + 'Gallery/Get';
+
+            return $http.get(url);
+        }
+    }
+})();
